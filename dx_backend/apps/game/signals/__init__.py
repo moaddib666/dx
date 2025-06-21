5a257e88d494818a1f60f58b72550b8642489b51
Max Nikitenko <max.nikitenko@namecheap.com>
2025-06-21 20:34:55 +0300

    feat(signals): rollback changes;

--- dx_backend/apps/game/signals/__init__.py
+++ dx_backend/apps/game/signals/__init__.py
@@ -8,15 +8,7 @@ notifier = BaseNotifier(event_bus)
 
 
 def on_character_changed(sender, instance, created, **kwargs):
-    # Skip notification if this is a creation (handle separately if needed)
-    if created:
-        notifier.character_changed(instance)
-        return
-
-    # Check if specific fields were updated
-    update_fields = kwargs.get('update_fields')
-    if not update_fields:
-        return
+    # TODO: decrease the number of calls to this function, by detecting changes by updated_fields and dirty fields
     notifier.character_changed(instance)
 
 
