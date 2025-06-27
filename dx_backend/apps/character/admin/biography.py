9f98206af89db7a88625e344ed226051f40aca05
Max Nikitenko <max.nikitenko@namecheap.com>
2025-06-27 20:02:09 +0300

    refactor(admin): update `CharacterBiographyAdmin` to extend `CampaignModelAdmin`
    Ensure `CharacterBiographyAdmin` filters queryset by active campaign context using session `campaign_id`. Add `get_queryset` override for consistent campaign-based filtering.

--- dx_backend/apps/character/admin/biography.py
+++ dx_backend/apps/character/admin/biography.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
+from apps.core.admin import CampaignModelAdmin
 from ..models import CharacterBiography
 
 
 @admin.register(CharacterBiography)
-class CharacterBiographyAdmin(admin.ModelAdmin):
+class CharacterBiographyAdmin(CampaignModelAdmin):
     """
     Admin interface for CharacterBiography.
     """
     list_display = ('character', 'age', 'gender', 'background', 'avatar_preview')
     search_fields = ('character__name', 'background', 'appearance')
     readonly_fields = ('avatar_preview',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        campaign_id = request.session.get('campaign_id')
+        if campaign_id:
+            return qs.filter(character__campaign_id=campaign_id)
+        return qs
 
     def avatar_preview(self, obj):
         if obj.avatar:
