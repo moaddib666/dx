8b48c9a0343d4c4cddb1282beb2e507eebd8567f
Max Nikitenko <max.nikitenko@namecheap.com>
2025-07-07 19:42:42 +0300

    feat(ui): add RPG avatar component for player profile
    Introduce `RPGAvatar` component with styled avatar display, placeholder image, and custom mask support. Includes responsive design and hover animations for improved interactivity.

---
+++ dx-frontend/src/components/PlayerRPGBars/RPGAvatar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="rpg-avatar">
+    <div class="rpg-avatar__wrapper">
+      <img
+          src="https://images.squarespace-cdn.com/content/v1/605e90f9c17ee25104dba783/1684973142360-DGM2VVQ5D1356WLMWZQD/Screenshot+2023-05-24+170440.png?format=1500w"
+          alt="Character Avatar"
+          class="rpg-avatar__image"
+          sizes="content"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.rpg-avatar {
+  width: 10rem;
+  height: 15rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  background: url("@/assets/rpg_bars/rpg_avatar_border.png") no-repeat center;
+  background-size: cover;
+
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  overflow: hidden;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+
+}
+
+.rpg-avatar__wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  mask: url("@/assets/rpg_bars/rpg_avatar_border_mask_invented.png") no-repeat center;
+  mask-size: cover;
+}
+
+.rpg-avatar__image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+</style>
