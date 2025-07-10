ab9d8aa0b2522677fcfe6d54a66734bde03ba3a4
Max Nikitenko <max.nikitenko@namecheap.com>
2025-07-10 10:24:01 +0300

    feat(ui): add ActionTriggerGroup component for grouped actions
    Introduce `ActionTriggerGroup` component to organize multiple `ActionTrigger` components into a grouped layout. Includes scoped styles for positioning and gap spacing. Facilitates a cohesive UI for related actions.

---
+++ dx-frontend/src/components/ActionArea/ActionTriggerGroup/ActionTriggerGroup.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+import ActionTrigger from "@/components/ActionArea/ActionTrigger/ActionTrigger.vue";
+</script>
+
+<template>
+<div class="trigger__group__holder">
+  <!-- EndTurn 100% - Left,Bottom -->
+  <ActionTrigger image="EndTurn" class="trigger__cta trigger__next-turn"/>
+  <!-- ToSafe 75%  Right, Bottom-->
+  <ActionTrigger image="ToSafe" class="trigger__cta trigger__safe-place"/>
+  <!-- RollDice 50% Left,Top -->
+  <ActionTrigger image="RollDice" class="trigger__cta trigger__roll-dice"/>
+</div>
+</template>
+
+<style scoped>
+.trigger__group__holder {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 1rem;
+  background: #00e5ff;
+}
+
+.trigger__next-turn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  left: 0;
+}
+.trigger__safe-place {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  bottom: 0;
+  right: 0;
+}
+.trigger__roll-dice {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+}
+</style>
