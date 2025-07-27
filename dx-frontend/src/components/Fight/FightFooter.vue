bafa8a77ae59237f8e5f9efdf37a882a4e03c68e
Max Nikitenko <max.nikitenko@namecheap.com>
2025-07-27 12:35:00 +0300

    feat(fight): add animated gradient effect to footer background
    Introduce a gradient animation effect over the crowd background in `FightFooter` to enhance visual appeal. Update styles to include masking, blur, and drop-shadow effects. Add new crowd texture asset for improved design.

--- dx-frontend/src/components/Fight/FightFooter.vue
+++ dx-frontend/src/components/Fight/FightFooter.vue
@@ -4,6 +4,10 @@
 
 <template>
   <div class="fight-footer">
+    <div class="background-container">
+      <div class="png-background-effect"></div>
+      <div class="png-background"></div>
+    </div>
   </div>
 </template>
 
@@ -13,8 +17,59 @@
   flex: 1;
   align-items: center;
   justify-content: center;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.background-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.png-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background: url('@/assets/fight/crowd.png') repeat-x center center;
   background-size: contain;
+}
+
+.png-background-effect {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    rgb(22, 234, 255),
+    rgb(253, 167, 53)
+  );
+  background-size: contain;
+  opacity: 0.5; /* Adjust opacity for the effect */
+  mask: url('@/assets/fight/crowd.png') repeat-x center center;
+  mask-size: contain;
+  transform: scale(1.005);
+  filter: blur(3rem) drop-shadow(0 0.2rem rgba(0, 0, 0, 0.6)) drop-shadow(0 0.1rem rgba(0, 0, 0, 0.4));
+  backdrop-filter: blur(3rem);
+  animation: gradientAnimation 5s linear infinite;
+  transition: all 0.3s ease;
+}
+
+@keyframes gradientAnimation {
+  0% {
+    background: linear-gradient(rgb(22, 234, 255), rgb(253, 167, 53));
+  }
+  50% {
+    background: linear-gradient(rgb(253, 167, 53), rgb(22, 234, 255));
+  }
+  100% {
+    background: linear-gradient(rgb(22, 234, 255), rgb(253, 167, 53));
+  }
 }
 </style>
