58c255b8417e5a13cc66a6d7e574bbb669677b00
Max Nikitenko <max.nikitenko@namecheap.com>
2025-08-27 17:36:40 +0300

    feat(models): add GMIconType enum for Game Master icons
    Introduce GMIconType enum to define various icon types used in the Game Master interface. Exported as default for easier imports.

---
+++ dx-frontend/src/models/GMIconTypes.ts
@@ -0,0 +1,13 @@
+// Game Master Icon Types
+// This enum defines the different types of icons used in the Game Master interface
+
+export enum GMIconType {
+  CHARACTER = 'character',
+  GAME_OBJECT = 'gameobject',
+  ITEM = 'items',
+  MAP = 'map',
+  SKILL = 'skill'
+}
+
+// Export the enum as default for easier imports
+export default GMIconType;
